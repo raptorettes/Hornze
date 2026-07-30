a3b8f54d79e1e4ea51a47b366d232f3f5103f36e
Keshet Karidi <allemakesfilms@gmail.com>
2026-07-31 00:23:31 +0200

    Tried to fix lazerbeam drifting from eye
    I failed, it's late, i go sleep now.

--- scenes/Lazer_Script.gd
+++ scenes/Lazer_Script.gd
@@ -2,11 +2,12 @@ extends Line2D
 
 @onready var start_point = $"../Eye".position
 @onready var end_point = $"../LazerEnd".position
+
 var point0 = points [0]
 var point1 = points [1]
 
 func _ready() -> void:
-	
+	$"../LazerEnd/Cloud_lazer/Lazer_Collision".disabled = true
 	point0 = start_point
 	point1 = start_point
 	set_point_position(0,point0)
@@ -18,9 +19,17 @@ func _process(delta: float) -> void:
 	var lazer = Input.is_action_just_pressed("Ability")
 	set_point_position(0,point0)
 	set_point_position(1,point1)
+	if Input.is_action_pressed("Ability"):
+		$"../LazerEnd/Cloud_lazer/Lazer_Collision".disabled = false
+	else:
+		$"../LazerEnd/Cloud_lazer/Lazer_Collision".disabled = true
 	if   lazer:
+		
+		$"../../AnimationPlayer".play("lazer-sfx")
 		var lazer_tween = get_tree().create_tween()
 		lazer_tween.tween_property(self,"point0",start_point,0.1)
 		lazer_tween.tween_property(self,"point1",end_point,0.1)
 		lazer_tween.tween_property(self,"point0",start_point,0.1)
 		lazer_tween.tween_property(self,"point1",start_point,0.1)
+
+		
--- scenes/cloud_gravity_logic.gd
+++ scenes/cloud_gravity_logic.gd
@@ -7,7 +7,9 @@ func _physics_process(delta: float) -> void:
 	pass # Replace with function body.
 
 
-func _on_cloud_enter_area_2d_area_entered(_area: Area2D) -> void:
+func _on_cloud_enter_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Lazer"):
+		queue_free()
 	var cloud_tween = get_tree().create_tween()
 	cloud_tween.set_trans(Tween.TRANS_LINEAR)
 	cloud_tween.set_parallel(false)
@@ -24,3 +26,4 @@ func _on_cloud_enter_area_2d_area_entered(_area: Area2D) -> void:
 	cloud_tween.tween_interval(0.1)
 
 	cloud_tween.tween_property(self, "linear_velocity", Vector2.ZERO, 0.4)
+	
