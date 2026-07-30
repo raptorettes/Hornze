6d901ae9f96110aa1a7d2afa9e42aa6488f76c71
Keshet Karidi <allemakesfilms@gmail.com>
2026-07-30 18:00:33 +0200

    questionable new glow effect on horse
    playing around with glow shader. kinda just copy pasted from a yt tutorial for now.

--- scenes/cloud_gravity_logic.gd
+++ scenes/cloud_gravity_logic.gd
@@ -1,23 +1,13 @@
 extends RigidBody2D
-@export var gravity_amount: float = 0
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	pass # Replace with function body.
-	#freeze = true
-	#freeze_mode = RigidBody2D.FREEZE_MODE_KINEMATIC
-
-
 
 func _physics_process(delta: float) -> void:
 	pass # Replace with function body.
-	#if _on_cloud_enter_area_2d_area_entered():
-		
+
 
 func _on_cloud_enter_area_2d_area_entered(_area: Area2D) -> void:
-	
-	
-	#freeze = false
-	#linear_velocity = Vector2.ZERO
 	var cloud_tween = get_tree().create_tween()
 	cloud_tween.set_trans(Tween.TRANS_LINEAR)
 	cloud_tween.set_parallel(false)
@@ -32,10 +22,5 @@ func _on_cloud_enter_area_2d_area_entered(_area: Area2D) -> void:
 	cloud_tween.tween_property(self, "gravity_scale", 0, 0.1)
 	
 	cloud_tween.tween_interval(0.1)
-#	cloud_tween.set_ease(Tween.EASE_OUT)
-	cloud_tween.tween_property(self, "linear_velocity", Vector2.ZERO, 0.4)
 
-		
-		
-	
-	#pass # Replace with function body.
+	cloud_tween.tween_property(self, "linear_velocity", Vector2.ZERO, 0.4)
