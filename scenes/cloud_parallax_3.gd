75f7c213bd567aa9ab52a7701f68c17cdc430ed1
Keshet Karidi <allemakesfilms@gmail.com>
2026-08-11 01:43:21 +0200

    middling success with extra clouds
    oof

---
+++ scenes/cloud_parallax_3.gd
@@ -0,0 +1,13 @@
+extends Parallax2D
+
+@onready var camera1: Camera2D = %Camera2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+	#scroll_scale = camera1.zoom
